f4d8707de3de5084b87c160a2acd1122ae222ebe
Paweł Grabiński <pawelrgrabinski@gmail.com>
2019-06-10 00:14:14 +0200

    Add new computation controlling scripts pt. 2. v. 2.

--- harmonic_unity_controller.py
+++ harmonic_unity_controller.py
@@ -6,7 +6,7 @@ for n in range(1, 7):
         naive_nde.train_plot_save(loss_function=naive_nde.loss_harmonic_unity, 
                                     eigen_value_function=naive_nde.eigen_value_harmonic,
                                     analytic_solution=naive_nde.harmonic_analytic,
-                                    name='Harmonic Oscilator',
+                                    name='Harmonic Oscilator Normalized',
                                     n=n, id=id, domain=(-4,4.), learning_rate=0.001
                                 )
         print('Done', n,id)
--- well_controller.py
+++ well_controller.py
@@ -6,7 +6,7 @@ for n in range(1, 3):
         naive_nde.train_plot_save(loss_function=naive_nde.loss_well, 
                                     eigen_value_function=naive_nde.eigen_value_well,
                                     analytic_solution=naive_nde.well_analytic,
-                                    name='Quantum Potential Well',
+                                    name='Quantum Potential Well Non-normalized',
                                     n=n, id=id, domain=(0,4.), learning_rate=0.001
                                 )
         print('Done', n,id)
