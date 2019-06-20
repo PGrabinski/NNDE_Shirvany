c8e8c5003053970d94cdb944dc2a7eaa3b03b029
Paweł Grabiński <pawelrgrabinski@gmail.com>
2019-06-20 16:08:24 +0200

    Add script for plot batch no. 5.

--- well_unity_controller.py
+++ well_unity_controller.py
@@ -1,6 +1,6 @@
 import naive_nde
 
-for n in range(3, 7):
+for n in range(5, 7):
     for id in range(20):
         print('Starting', n, id)
         naive_nde.train_plot_save(loss_function=naive_nde.loss_well_unity, 
@@ -9,6 +9,6 @@ for n in range(3, 7):
                                     name='Quantum Potential Well',
                                     dir='Quantum_Potential_Well_Probability_Loss',
                                     n=n, id=id, domain=(0,4.), learning_rate=0.001,
-                                    normalize=False, probability_weight=10
+                                    normalize=False, probability_weight=100
                                 )
         print('Done', n, id)
