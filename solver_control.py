9b0fd0c09aa0fdee38fe5935af0534cd4954b29c
Pawel <pawelrgrabinski@gmail.pl>
2019-07-02 16:54:55 +0200

    Add search results.

--- solver_control.py
+++ solver_control.py
@@ -2,7 +2,7 @@ import naive_nde as nnde
 import solver
 import time
 X_train, X_test = nnde.train_test_domain_a_to_b(0, 4.)
-solving_machine = solver.Solver(tolerance=1e-5, delta_E=1e-2, max_iter=10000, max_eigen_counter=1e3, n_h=100)
+solving_machine = solver.Solver(tolerance=1e-5, delta_E=1e-2, max_iter=25000, max_eigen_counter=1e2, n_h=100)
 eigen_value, network = solving_machine.solve(X=X_train,
     conditions=nnde.zero_boundary_conditions(0, 4.), loss_function=nnde.loss_well_unity, learning_rate=0.001)
 timestamp = time.time()
